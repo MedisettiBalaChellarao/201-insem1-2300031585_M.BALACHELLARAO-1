e2af5e1f6983432fe45774450eb7a1196f52ff78
MedisettiBalaChellarao <2300031585@kluniversity.in>
2025-10-25 08:14:41 +0530

    first

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/s201-insem1-2300031585_M.BALACHELLARAO-1/",
+
+  plugins: [react()],
+})
