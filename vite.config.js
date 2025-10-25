3ab1deee5530013f32462078a2afa76bd282dda6
MEDISETTI BALA CHELLARAO <2300031585@kluniversity.in>
2025-10-25 08:22:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/s201-insem1-2300031585_M.BALACHELLARAO-1/",
+  base: "/201-insem1-2300031585_M.BALACHELLARAO-1/",
 
   plugins: [react()],
 })
